bfa4277baaf9b84c85e45822e469e010b05ef3cc
HatsuSixty <hatsunotsus@gmail.com>
2024-01-19 14:25:26 -0300

    Allow users to share links to pages

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const DEFAULT_PAGE = "pages/home.html";
+var currentPage = DEFAULT_PAGE;
+
+function sharePage() {
+    const url = window.location.origin;
+    navigator.clipboard.writeText(url + '/?page=' + encodeURI(currentPage));
+    alert('The generated link was copied to the clipboard!');
+}
 
 function updatePage(pg) {
     let content = document.getElementById("content");
     content.innerHTML = "<h1>Loading...</h1>";
     fetch(pg).then(x => {
         x.text().then(x => {
-            content.innerHTML = x;
+            const sharePageLink =
+                  `<a class="sharelink" href="javascript:sharePage()">
+                     Share this page!
+                   </a><br>`;
+            content.innerHTML = sharePageLink + x + "<br><br>" + sharePageLink;
         });
     });
+    currentPage = pg;
 }
 
-updatePage(DEFAULT_PAGE);
+{
+    const params = new URLSearchParams(window.location.search);
+    const pageParam = params.get("page");
+    if (pageParam) {
+        updatePage(decodeURI(pageParam));
+    } else {
+        updatePage(DEFAULT_PAGE);
+    }
+}
