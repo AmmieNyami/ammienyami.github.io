986e9987360c024cf99c14c46292f071c9ac99ca
HatsuSixty <hatsunotsus@gmail.com>
2024-01-24 23:15:22 -0300

    Introduce themes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function sharePage() {
 }
 
 function updatePage(pg) {
-    let content = document.getElementById("content");
+    const content = document.getElementById("content");
     content.innerHTML = "<h1>Loading...</h1>";
     fetch(pg).then(x => {
         x.text().then(x => {
@@ -24,7 +24,52 @@ function updatePage(pg) {
     currentPage = pg;
 }
 
+function setTheme(theme) {
+    const style = document.documentElement.style;
+    style.setProperty("--gap", "25px");
+    style.setProperty("--header-title-padding", "50px");
+    style.setProperty("--header-link-padding", "15px");
+    style.setProperty("--content-padding", "25px");
+    style.setProperty("--body-padding", "20px");
+
+    const themes = {
+        "normal": {
+            "background-bright-color": "#933c73",
+            "background-dark-color": "#7B3260",
+            "content-border-color": "#2F1325",
+            "content-background-color": "#B74B90",
+            "content-foreground-color": "#FFFFFF",
+            "link-background-color": "#ff69c9"
+        },
+        "simple": {
+            "background-bright-color": "#d8d8d8",
+            "background-dark-color": "#e7e7e7",
+            "content-border-color": "#000000",
+            "content-background-color": "#FFFFFF",
+            "content-foreground-color": "#000000",
+            "link-background-color": "#FFFF00"
+        },
+        "dark": {
+            "background-bright-color": "#272727",
+            "background-dark-color": "#181818",
+            "content-border-color": "#FFFFFF",
+            "content-background-color": "#000000",
+            "content-foreground-color": "#FFFFFF",
+            "link-background-color": "#000000"
+        }
+    };
+
+    const currentTheme = themes[theme];
+    for (const [key, value] of Object.entries(currentTheme)) {
+        style.setProperty("--" + key, value);
+    }
+
+    localStorage.setItem("lastTheme", theme);
+}
+
 {
+    setTheme(localStorage.getItem("lastTheme") || "normal");
+
     const params = new URLSearchParams(window.location.search);
     const pageParam = params.get("page");
     if (pageParam) {
