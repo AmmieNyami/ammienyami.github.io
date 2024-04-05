c6ca8d7ccb506bce41c24e6ba6ff0e1e9c5a3d55
HatsuSixty <hatsunotsus@gmail.com>
2024-04-05 16:32:11 -0300

    Add multiple themes!

---
+++ themes/hatsusixtys-theme/static/index.js
@@ -0,0 +1,29 @@
+function setTheme(theme) {
+    const style = document.documentElement.style;
+
+    const themes = {
+        "modern": {
+            "code-font": "\"Lucida Console\", Monaco, monospace",
+            "normal-font": "Optima, Candara, Calibri, Arial, sans-serif",
+            "background-image": "none lmao",
+            "content-background-color": "#FFFFFF",
+            "content-border-color": "#FFFFFF",
+        },
+        "cool": {
+            "code-font": "\"Lucida Console\", Monaco, monospace",
+            "normal-font": "monospace",
+            "background-image": "url(/cool-background.png)",
+            "content-background-color": "#D8D8D8",
+            "content-border-color": "#B74B90",
+        },
+    };
+
+    const currentTheme = themes[theme];
+    for (const [key, value] of Object.entries(currentTheme)) {
+        style.setProperty("--" + key, value);
+    }
+
+    localStorage.setItem("lastTheme", theme);
+}
+
+setTheme(localStorage.getItem("lastTheme") || "modern");
