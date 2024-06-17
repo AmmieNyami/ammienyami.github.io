61d74f6023a693930b992a3b58ed21a8f6671e9e
HatsuSixty <hatsunotsus@gmail.com>
2024-06-16 23:51:33 -0300

    Add new theme

--- themes/hatsusixtys-theme/static/index.js
+++ themes/hatsusixtys-theme/static/index.js
@@ -1,13 +1,22 @@
+const default_theme = "dark";
+
 function setTheme(theme) {
     const style = document.documentElement.style;
 
     const defaults = {
+        "foreground-color": "#000000",
+        "background-color": "#FFFFFF",
         "header-link-color": "#B74B90",
         "header-foreground-color": "#FFFFFF",
         "header-background-color": "#B74B90",
         "header-border-color": "#FFBEE7",
+
         "accent-color": "#B74B90",
         "accent-alt-color": "#FFBEE7",
+
+        "hr-color": "#ddd",
+        "link-color": "blue",
+        "background-image": "none lmao",
     };
 
     const themes = {
@@ -15,10 +24,8 @@ function setTheme(theme) {
             "code-font": "\"Lucida Console\", Monaco, monospace",
             "normal-font": "Optima, Candara, Calibri, Arial, sans-serif",
 
-            "background-image": "none lmao",
             "content-background-color": "#FFFFFF",
             "content-border-color": "#FFFFFF",
-            "hr-color": "#ddd",
         },
         "minimal": {
             "header-background-color": "#FFF9FD",
@@ -28,10 +35,24 @@ function setTheme(theme) {
             "code-font": "\"Lucida Console\", Monaco, monospace",
             "normal-font": "Optima, Candara, Calibri, Arial, sans-serif",
 
-            "background-image": "none lmao",
             "content-background-color": "#FFFFFF",
             "content-border-color": "#FFFFFF",
-            "hr-color": "#ddd",
+        },
+        "dark": {
+            "header-background-color": "#62274e",
+            "header-border-color": "#FFD0ED",
+            "header-foreground-color": "#FFD0ED",
+
+            "code-font": "\"Lucida Console\", Monaco, monospace",
+            "normal-font": "Optima, Candara, Calibri, Arial, sans-serif",
+
+            "accent-alt-color": "#ff82d5",
+
+            "background-color": "#3C1830",
+            "content-background-color": "#3C1830",
+            "content-border-color": "#3C1830",
+            "foreground-color": "#FFFFFF",
+            "link-color": "#FFFF20",
         },
         "cool": {
             "code-font": "\"Lucida Console\", Monaco, monospace",
@@ -43,6 +64,10 @@ function setTheme(theme) {
             "hr-color": "#8f8f8f",
         },
     };
+
+    if (!(theme in themes)) {
+        theme = default_theme;
+    }
 
     const currentTheme = themes[theme];
     for (const [key, value] of Object.entries(currentTheme)) {
@@ -58,4 +83,4 @@ function setTheme(theme) {
     localStorage.setItem("lastTheme", theme);
 }
 
-setTheme(localStorage.getItem("lastTheme") || "minimal");
+setTheme(localStorage.getItem("lastTheme") || default_theme);
